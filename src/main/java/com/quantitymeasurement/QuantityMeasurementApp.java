f17f57cc00f7be14802766949789481679eed7d8
RaviKumar-1908 <mr.ravikr213@gmail.com>
2026-02-23 08:38:11 +0530

    [Ravi Kumar] : Modified QuantityMeasurementApp file

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -15,6 +15,15 @@ public class QuantityMeasurementApp {
 		Feet f1 = new Feet(inputOne);
         Feet f2 = new Feet(inputTwo);
         System.out.println("Are equal? " + f1.equals(f2));
+        // Inches Measurement 
+        System.out.println("Enter First Entry (Inch) : ");
+        double inputOneInch = sc.nextDouble();
+        System.out.println("Enter Second Entry (Inch) : ");
+        double inputTwoInch = sc.nextDouble();
+        Inches i1 = new Inches(inputOneInch);
+        Inches i2 = new Inches(inputTwoInch);
+        System.out.println("Are equal? " + i1.equals(i2));
+	
         
 	}
 }
